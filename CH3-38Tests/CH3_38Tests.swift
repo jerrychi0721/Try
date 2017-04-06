128d528116a36aacf61ef91c5edfae19135994f6
JerryChi <ttt210200048@gmail.com>
2017-04-07 00:43:13 +0800

    Initial Commit

---
+++ CH3-38Tests/CH3_38Tests.swift
@@ -0,0 +1,36 @@
+//
+//  CH3_38Tests.swift
+//  CH3-38Tests
+//
+//  Created by JerryChi on 2017/4/7.
+//  Copyright © 2017年 JerryChi. All rights reserved.
+//
+
+import XCTest
+@testable import CH3_38
+
+class CH3_38Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
